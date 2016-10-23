67c6691a81e62b088474d0b19729d0060e83b12e
Yuya Kaido <yuyakaido@gmail.com>
2016-10-23 18:25:44 +0900

    Handle runtime permission in sample app

--- sample/src/main/java/com/yuyakaido/android/rxmedialoader/sample/MainActivity.java
+++ sample/src/main/java/com/yuyakaido/android/rxmedialoader/sample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yuyakaido.android.rxmedialoader.sample;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,16 +13,21 @@ import com.yuyakaido.android.rxmedialoader.RxMediaLoader;
 
 import java.util.List;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 import rx.Subscriber;
 
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+
+    private FolderListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FolderListAdapter adapter = new FolderListAdapter(this);
+        adapter = new FolderListAdapter(this);
         ListView listView = (ListView) findViewById(R.id.activity_main_list_view);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -31,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        MainActivityPermissionsDispatcher.loadWithCheck(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    public void load() {
         RxMediaLoader.medias(this, getSupportLoaderManager())
                 .subscribe(new Subscriber<List<Folder>>() {
                     @Override
