39ba52eb2c77f2176c97d3cbda143e3764833d82
Yuya Kaido <yuyakaido@gmail.com>
2016-10-24 10:21:33 +0900

    Add api to load only photo or video

--- rxmedialoader/src/main/java/com/yuyakaido/android/rxmedialoader/RxMediaLoader.java
+++ rxmedialoader/src/main/java/com/yuyakaido/android/rxmedialoader/RxMediaLoader.java
@@ -16,58 +16,84 @@ public class RxMediaLoader {
 
     public static Observable<List<Folder>> medias(
             final Context context, final LoaderManager loaderManager) {
+        return folders(context, loaderManager)
+                .flatMap(photosFunc(context, loaderManager))
+                .flatMap(videosFunc(context, loaderManager));
+    }
+
+    public static Observable<List<Folder>> photos(
+            final Context context, final LoaderManager loaderManager) {
+        return folders(context, loaderManager)
+                .flatMap(photosFunc(context, loaderManager));
+    }
+
+    public static Observable<List<Folder>> videos(
+            final Context context, final LoaderManager loaderManager) {
+        return folders(context, loaderManager)
+                .flatMap(videosFunc(context, loaderManager));
+    }
+
+    private static Observable<List<Folder>> folders(
+            final Context context, final LoaderManager loaderManager) {
         if (!PermissionUtil.hasReadExternalStoragePermission(context)) {
             return Observable.error(new NeedPermissionException(
                     "This operation needs android.permission.READ_EXTERNAL_STORAGE"));
         }
 
-        return Observable.create(
-                new Observable.OnSubscribe<List<Folder>>() {
+        return Observable.create(new Observable.OnSubscribe<List<Folder>>() {
+            @Override
+            public void call(final Subscriber<? super List<Folder>> subscriber) {
+                new FolderLoader(context, loaderManager, new FolderLoader.Callback() {
+                    @Override
+                    public void onFolderLoaded(List<Folder> folders) {
+                        subscriber.onNext(folders);
+                        subscriber.onCompleted();
+                    }
+                });
+            }
+        });
+    }
+
+    private static Func1<List<Folder>, Observable<List<Folder>>> photosFunc(
+            final Context context, final LoaderManager loaderManager) {
+        return new Func1<List<Folder>, Observable<List<Folder>>>() {
+            @Override
+            public Observable<List<Folder>> call(final List<Folder> folders) {
+                return Observable.create(new Observable.OnSubscribe<List<Folder>>() {
                     @Override
                     public void call(final Subscriber<? super List<Folder>> subscriber) {
-                        new FolderLoader(context, loaderManager, new FolderLoader.Callback() {
+                        new PhotoLoader(context, loaderManager, folders, new PhotoLoader.Callback() {
                             @Override
-                            public void onFolderLoaded(List<Folder> folders) {
+                            public void onPhotoLoaded() {
                                 subscriber.onNext(folders);
                                 subscriber.onCompleted();
                             }
                         });
                     }
-                })
-                .flatMap(new Func1<List<Folder>, Observable<List<Folder>>>() {
+                });
+            }
+        };
+    }
+
+    private static Func1<List<Folder>, Observable<List<Folder>>> videosFunc(
+            final Context context, final LoaderManager loaderManager) {
+        return new Func1<List<Folder>, Observable<List<Folder>>>() {
+            @Override
+            public Observable<List<Folder>> call(final List<Folder> folders) {
+                return Observable.create(new Observable.OnSubscribe<List<Folder>>() {
                     @Override
-                    public Observable<List<Folder>> call(final List<Folder> folders) {
-                        return Observable.create(new Observable.OnSubscribe<List<Folder>>() {
+                    public void call(final Subscriber<? super List<Folder>> subscriber) {
+                        new VideoLoader(context, loaderManager, folders, new VideoLoader.Callback() {
                             @Override
-                            public void call(final Subscriber<? super List<Folder>> subscriber) {
-                                new PhotoLoader(context, loaderManager, folders, new PhotoLoader.Callback() {
-                                    @Override
-                                    public void onPhotoLoaded() {
-                                        subscriber.onNext(folders);
-                                        subscriber.onCompleted();
-                                    }
-                                });
-                            }
-                        });
-                    }
-                })
-                .flatMap(new Func1<List<Folder>, Observable<List<Folder>>>() {
-                    @Override
-                    public Observable<List<Folder>> call(final List<Folder> folders) {
-                        return Observable.create(new Observable.OnSubscribe<List<Folder>>() {
-                            @Override
-                            public void call(final Subscriber<? super List<Folder>> subscriber) {
-                                new VideoLoader(context, loaderManager, folders, new VideoLoader.Callback() {
-                                    @Override
-                                    public void onVideoLoaded() {
-                                        subscriber.onNext(folders);
-                                        subscriber.onCompleted();
-                                    }
-                                });
+                            public void onVideoLoaded() {
+                                subscriber.onNext(folders);
+                                subscriber.onCompleted();
                             }
                         });
                     }
                 });
+            }
+        };
     }
 
 }
